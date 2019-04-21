6b55243370a312d5868b6e215ba9fe2bf68fe414
Chua Hou <extremelyspicystudios@gmail.com>
2019-04-21 16:47:52 +0800

    Add notification capability

--- app/src/main/java/dev/chuahou/fgo_ap_reminder/MainActivity.java
+++ app/src/main/java/dev/chuahou/fgo_ap_reminder/MainActivity.java
@@ -1,5 +1,13 @@
 package dev.chuahou.fgo_ap_reminder;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +16,6 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Fate/AP calculator");
+
+        // create notification channel
+        CharSequence name = "Notification Channel";
+        String description = "Notification Channel";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("0", name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
     public void buttonClicked(View view) {
@@ -29,11 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
         // get AP values
         int currentAP, desiredAP, maxAP;
-        try {
+        try
+        {
             currentAP = Integer.parseInt(currentAPEditText.getText().toString());
             desiredAP = Integer.parseInt(desiredAPEditText.getText().toString());
             maxAP = Integer.parseInt(maxAPEditText.getText().toString());
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             outputTextView.setText("ちゃんと入力してくださいよ、マスター。");
             return;
         }
@@ -64,6 +82,32 @@ public class MainActivity extends AppCompatActivity {
 
         // set text view text
         outputTextView.setText(outputText.toString());
+
+        // schedule notifications
+        NotificationCompat.Builder builder1 = new NotificationCompat.Builder(this, "0")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("行きましょう、マスター。")
+                .setContentText("APが足りました。決着をつけましょう。");
+        scheduleNotification(1, builder1.build(), minToDesiredAP);
+        NotificationCompat.Builder builder2 = new NotificationCompat.Builder(this, "0")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("まだですか？マスター。")
+                .setContentText("そろそろ行った方がいいではないですか？");
+        scheduleNotification(2, builder2.build(), minToMaxAP);
+    }
+
+    private void scheduleNotification(int id, Notification n, int delay) {
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, id);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, n);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, id,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Calendar time = Calendar.getInstance();
+        time.setTimeInMillis(System.currentTimeMillis());
+        time.add(Calendar.MINUTE, delay);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), pendingIntent);
     }
 
 }
