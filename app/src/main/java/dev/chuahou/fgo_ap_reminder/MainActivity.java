16068277d97fe98a5b031e1672fc4810acacc10b
Chua Hou <extremelyspicystudios@gmail.com>
2019-04-25 21:08:16 +0800

    Add saving of current state, refactor MainActivity into modular functions

--- app/src/main/java/dev/chuahou/fgo_ap_reminder/MainActivity.java
+++ app/src/main/java/dev/chuahou/fgo_ap_reminder/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
@@ -18,14 +19,30 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    // required views from this activity
+    private TextView _outputTextView;
+    private EditText _currentAPEditText;
+    private EditText _desiredAPEditText;
+    private EditText _maxAPEditText;
+
+    // shared preferences for saving and keys
+    private SharedPreferences _sharedPreferences;
+    private final String _SP_TIME_KEY = "time";
+    private final String _SP_CURRENT_AP_KEY = "current";
+    private final String _SP_DESIRED_AP_KEY = "desired";
+    private final String _SP_MAX_AP_KEY = "max";
+
+    // initialize the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        // set content and title
+        setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle(R.string.actionBarTitle);
 
         // create notification channel
@@ -36,28 +53,31 @@ public class MainActivity extends AppCompatActivity {
         channel.setDescription(description);
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(channel);
+
+        // get needed views
+        _outputTextView = (TextView) findViewById(R.id.outputTextView);
+        _currentAPEditText = (EditText) findViewById(R.id.currentAPEditText);
+        _desiredAPEditText = (EditText) findViewById(R.id.desiredAPEditText);
+        _maxAPEditText = (EditText) findViewById(R.id.maxAPEditText);
+
+        // get shared preferences
+        _sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        Calendar c = new GregorianCalendar();
+        c.setTimeInMillis(_sharedPreferences.getLong(_SP_TIME_KEY, 0));
+        int currentAP = _sharedPreferences.getInt(_SP_CURRENT_AP_KEY, 0);
+        int desiredAP = _sharedPreferences.getInt(_SP_DESIRED_AP_KEY, 0);
+        int maxAP = _sharedPreferences.getInt(_SP_MAX_AP_KEY, 0);
+
+        // set output text and fill fields
+        updateState(currentAP, desiredAP, maxAP, false);
+        _currentAPEditText.setText(String.valueOf(currentAP));
+        _desiredAPEditText.setText(String.valueOf(desiredAP));
+        _maxAPEditText.setText(String.valueOf(maxAP));
     }
 
-    public void buttonClicked(View view) {
-        // get relevant UI components
-        TextView outputTextView = (TextView) findViewById(R.id.outputTextView);
-        EditText currentAPEditText = (EditText) findViewById(R.id.currentAPEditText);
-        EditText desiredAPEditText = (EditText) findViewById(R.id.desiredAPEditText);
-        EditText maxAPEditText = (EditText) findViewById(R.id.maxAPEditText);
-
-        // get AP values
-        int currentAP, desiredAP, maxAP;
-        try
-        {
-            currentAP = Integer.parseInt(currentAPEditText.getText().toString());
-            desiredAP = Integer.parseInt(desiredAPEditText.getText().toString());
-            maxAP = Integer.parseInt(maxAPEditText.getText().toString());
-        } catch (Exception e)
-        {
-            outputTextView.setText(R.string.invalidInputMessage);
-            return;
-        }
-
+    // process AP levels, updating the saved state, notification and output text
+    private void updateState(int currentAP, int desiredAP, int maxAP, boolean notify)
+    {
         // calculate how much time to each AP level
         int minToDesiredAP = 5 * Math.max(desiredAP - currentAP, 0);
         int minToMaxAP = 5 * Math.max(maxAP - currentAP, 0);
@@ -70,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         // get current time
         Calendar c = Calendar.getInstance();
+        long currTimeInMillis = c.getTimeInMillis(); // for saving
         outputText.append(getString(R.string.currentTime) + df.format(c.getTime()) + "\n");
 
         // get time to desired AP
@@ -94,25 +115,55 @@ public class MainActivity extends AppCompatActivity {
         outputText.append(getString(R.string.timeAtMax) + df.format(c.getTime()) + "\n");
 
         // set text view text
-        outputTextView.setText(outputText.toString());
+        _outputTextView.setText(outputText.toString());
 
         // schedule notifications
-        Bitmap saber_stand = BitmapFactory.decodeResource(getResources(), R.drawable.saber_stand);
-        Bitmap saber_sad = BitmapFactory.decodeResource(getResources(), R.drawable.saber_sad);
-        NotificationCompat.Builder builder1 = new NotificationCompat.Builder(this, "0")
-                .setSmallIcon(R.drawable.saber_notif)
-                .setLargeIcon(saber_stand)
-                .setContentTitle(getString(R.string.desiredNotifTitle))
-                .setContentText(getString(R.string.desiredNotifText));
-        scheduleNotification(1, builder1.build(), minToDesiredAP);
-        NotificationCompat.Builder builder2 = new NotificationCompat.Builder(this, "0")
-                .setSmallIcon(R.drawable.saber_notif)
-                .setLargeIcon(saber_sad)
-                .setContentTitle(getString(R.string.maxNotifTitle))
-                .setContentText(getString(R.string.maxNotifText));
-        scheduleNotification(2, builder2.build(), minToMaxAP);
+        if (notify) {
+            Bitmap saber_stand = BitmapFactory.decodeResource(getResources(), R.drawable.saber_stand);
+            Bitmap saber_sad = BitmapFactory.decodeResource(getResources(), R.drawable.saber_sad);
+            NotificationCompat.Builder builder1 = new NotificationCompat.Builder(this, "0")
+                    .setSmallIcon(R.drawable.saber_notif)
+                    .setLargeIcon(saber_stand)
+                    .setContentTitle(getString(R.string.desiredNotifTitle))
+                    .setContentText(getString(R.string.desiredNotifText));
+            scheduleNotification(1, builder1.build(), minToDesiredAP);
+            NotificationCompat.Builder builder2 = new NotificationCompat.Builder(this, "0")
+                    .setSmallIcon(R.drawable.saber_notif)
+                    .setLargeIcon(saber_sad)
+                    .setContentTitle(getString(R.string.maxNotifTitle))
+                    .setContentText(getString(R.string.maxNotifText));
+            scheduleNotification(2, builder2.build(), minToMaxAP);
+        }
+
+        // save shared preferences
+        SharedPreferences.Editor editor = _sharedPreferences.edit();
+        editor.putLong(_SP_TIME_KEY, currTimeInMillis);
+        editor.putInt(_SP_CURRENT_AP_KEY, currentAP);
+        editor.putInt(_SP_DESIRED_AP_KEY, desiredAP);
+        editor.putInt(_SP_MAX_AP_KEY, maxAP);
+        editor.commit();
     }
 
+    // handle confirm button clicked
+    public void buttonClicked(View view) {
+        // get AP values
+        int currentAP, desiredAP, maxAP;
+        try
+        {
+            currentAP = Integer.parseInt(_currentAPEditText.getText().toString());
+            desiredAP = Integer.parseInt(_desiredAPEditText.getText().toString());
+            maxAP = Integer.parseInt(_maxAPEditText.getText().toString());
+        } catch (Exception e)
+        {
+            _outputTextView.setText(R.string.invalidInputMessage);
+            return;
+        }
+
+        // update state and notify
+        updateState(currentAP, desiredAP, maxAP, true);
+    }
+
+    // schedules provided notification in specified number of minutes
     private void scheduleNotification(int id, Notification n, int delay) {
         Intent notificationIntent = new Intent(this, NotificationPublisher.class);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, id);
